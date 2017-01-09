ee9f1e8d2f8cbe86e55682c105843dae1b15c4ff
Mugenx <sahawu.mail@gmail.com>
2017-01-08 22:50:25 -0500

    Contact page update

--- views/contact.php
+++ views/contact.php
@@ -1,4 +1,22 @@
 <div class="main">
+<div class="container" style="width: 900px; padding: 0 0 50px;  margin: 50px auto; box-shadow: 0 0 2rem rgba(0, 0, 0, 0.3);" align="center">
+
+<div id="map" style="width:100%;height:300px"></div>
+  <script>
+    function initMap() {
+      var uluru = {lat: 45.351881, lng: -75.788592};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    }
+  </script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZukZhiHna2oD9IlBqP0MDD5vCUJkVRqY&callback=initMap"
+  type="text/javascript"></script>
 
 	<?php 
 		if(isset($success) && $success == 1 && isset($_POST)){
@@ -9,6 +27,9 @@
 		}
 	
 	?>
+	
+	<div style="width: 700px; margin:0 auto">
+		
 	
 	
 	<h2 class="form-signin-heading"><?php echo TextSelector("Contact Us", "Contactez Nous"); ?></h2>
@@ -44,6 +65,7 @@
 			<div class="col-md-12">
 				<button class="btn btn-lg btn-primary btn-block" type="submit" name="send_message" id="send_message"><?php echo TextSelector("Send Message", "Envoyer"); ?></button>
 			</div>
+<p>fields marked with an "*" are required</p>
 		</form>
 
 <script>
@@ -79,5 +101,8 @@ $("#contactForm").validate({
       }
 });
 </script>
-
+</div>
+</div>
+<div class="rTop" id="rTop" onClick="toTop()"><span class="glyphicon glyphicon-chevron-up" style="font-size: 20px; padding: 10px 0; 
+    " ></span></div>
 </div>
