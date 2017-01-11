85a6d7da9893d3a4d04aa4e3cd3f7da935f14d2c
Mugenx <sahawu.mail@gmail.com>
2017-01-11 12:54:07 -0500

    Header and index update

---
+++ index.php
@@ -0,0 +1,291 @@
+<div  align="center" style="margin: 20px auto -60px">
+  <!-- Big Frame -->
+  <div class="frame" align="center" style="box-shadow: 0 0 2rem rgba(0, 0, 0, 0.3);">
+    <!-- A -->
+    <div class="a" id="a">
+      <div class="arrow-left"></div>
+      <p id="aTitle" style="margin:20px; margin-top:100px">
+        <span style="text-align: center; color: white; font-size: 30px ">
+          EventSystemPro
+        </span><br>
+        <div style="width:100px;height: 1px; background-color: white"></div><br>
+        <span style="color: white; ">
+          Built by Event Planners for Event Planners
+        </span>
+      </p>
+      <div class="aover" style="margin-top: 20px; width: 600px; height: 150px; background-color: rgba(0, 0, 0, .3); display: none; z-index: 0" align="center">
+        <a class="btn btn-primary" align="center" style="margin-top: 50px; opacity:1" href="/verneyhome/about">Learn more</a>
+      </div>
+    </div>
+    <!-- A -->
+
+    <!-- B -->
+    <div class="b" id="b">
+      <p class="bTitle" style="margin-top: 200px;">
+        <span style="color: white; font-size: 30px">
+          What is ESP?
+        </span>
+
+        <div style="width:60px; height: 1px; background-color: white"></div>
+      </p>
+      <p class="bSub" style="color: white; float: left; margin-top: 10px; text-align: center; padding: 0 18px">
+        A unique combination of online event management related tools
+      </p>
+      <div class="bover" style="margin-top: 147px; width: 300px; height: 300px; background-color: rgba(0, 0, 0, .3); display: none; z-index: 0" align="center">
+        <a class="btn btn-primary" align="center" style="margin-top: 130px; opacity:1" href="/verneyhome/services">Learn more</a>
+      </div>
+    </div>
+    <!-- B -->
+
+    <!-- C -->
+    <div class="c" id="c" style="background-color: #b78500">
+     <p class="cTitle" align="center" style="margin:100px 30px 10px; ">
+      <span style="color: white; font-size: 30px;">
+        Our Business Model
+      </span>
+      <div style="width:100px;height: 1px; background-color: white"></div><br>
+      <span style=" color: white;">Choose between Organizational or Reseller Options</span>
+    </p>
+    <div class="cover" style="margin-top: 20px; width: 300px; height: 136px; background-color: rgba(0, 0, 0, .3);  display: none; z-index: 0" align="center">
+      <a class="btn btn-primary" align="center" style="margin-top: 50px; opacity:1" href="/verneyhome/eventClients">Learn more</a>
+    </div>
+  </div>
+  <!-- C -->
+
+  <!-- D -->
+  <div class="d" id="d" style="background-color: white">
+    <div class="arrow-right" style="z-index: 1; position: absolute; border-left: 10px solid #b78500;"></div>
+    <div class="dTitle" align="center" style="margin-top: 100px; z-index: 10">
+      <p>
+        <span style="color: #003366; font-size: 30px;">
+          Try it Out!
+        </span>
+      </p>
+      <div style="width:100px;height: 1px; background-color: #003366;"></div>
+      <p style="color: #003366; margin-top: 20px;">
+        Get a taste of your participant's experience  
+      </p>
+      <div class="dover" style="margin-top: 20px; width: 300px; height: 137px; background-color: rgba(0, 0, 0, .3); display: none; z-index: 0" align="center">
+        <a class="btn btn-primary" align="center" style="margin-top: 50px; opacity:1" href="/verneyhome/becomePartner">Learn more</a>
+      </div>
+    </div>
+  </div>
+  <!-- D -->
+</div>
+<!-- End of Big Frame -->
+
+<!-- small frame -->
+<div class="sframe">
+  <div class="aa">
+    <P class="aaT">
+      Verney Conference Management
+    </P>
+    <p class="aaS">
+      We specialize in the human side of communication. Regardless of how fast technology allows us to communicate in our daily lives, nothing will ever replace the value of learning from like-minded individuals face-to-face. It is our goal to bring conferences, seminars, workshops or any learning event to the next level, providing you with true information exchange solutions.
+    </p>
+  </div>
+  <div class="bb">
+    <p class="bbT">
+      Services
+    </p>
+    <p class="bbS">
+      Let us help you produce a top quality event specific to your organization's needs or join us for one of our own independently researched events. 
+    </p>
+  </div>
+  <div class="cc">
+    <p class="ccT">
+      Events & Clients
+    </p>
+    <p class="ccS">
+      We produce independently researched, job-specific or industry-focused learning events that are available to the public. Our past and upcoming events can be seen here.
+    </p>
+
+  </div>
+  <div class="dd">
+    <p class="ddT">
+      Become a Partner
+    </p>
+    <p class="ddS">
+      As the public sector evolves, ensure that your company has an established brand and the personal relationships needed to win public sector business.
+    </p>
+  </div>
+</div>
+<!-- end of small frame -->
+</div>
+
+<div id="about" class="container-fluid" style="padding: 25px 0; margin-top: 100px; color:white; background-color: #003366" align="center">
+  <span style="font-size: 5rem; margin-bottom: 50px;">Our History</span>
+  <div class="col-lg-10" style="float: none; margin: 0 auto" >
+    
+    <img src="img/IMG_8136ac.png" style="width: 250px; float: left; margin-right:40px">
+
+      <p style="color: white; text-align: justify;">
+        Verney Conference Management (“VCM”) was founded in 2002. Over the past 
+        15 years, the company has grown to produce over 250 events from coast to 
+        coast.<br><br>
+
+        Dustin Rivers, the company's co-Founder, was working in Toronto for what 
+        he calls a "conference production factory, that prioritized quantity of 
+        events over quality of agenda, with little regard for the communities 
+        the events were supposed to serve". He was quickly able to identify an 
+        opportunity, in large part due to the feedback he was receiving about 
+        the conferences he was currently producing. With an eye towards working 
+        with the public sector to produce the same type of high quality events, 
+        but at drastically less cost per delegate, Dustin packed up his meager 
+        belongings and moved to Ottawa to start Verney Conference Management.<br><br>
+
+        It wasn't until 2003 that things really began to take off, and Dustin 
+        began formulating a new Event Production Business Model - what he now 
+        calls Community-Based Developed events. In April of that year, VCM was 
+        joined by now Partner and Co-owner, Chris Lutz.<br><br>
+
+        Over this time, as Dustin and Chris honed their negotiation skills and 
+        learned more about the financial intricacies of the event industry. VCM 
+        took a "let's try to do it ourselves before paying someone else" 
+        mentality and quickly became a one-stop shop for complete Event 
+        Production.  Not only did the team at VCM manage event logistics along 
+        with its associated vendors, it procured an AV arsenal capable of 
+        servicing a 300+ person event with 5 concurrent sessions running at any 
+        given time. VCM also began supplying in-house Graphic Design services, 
+        but perhaps most impressively of all, they created what is now known as 
+        EventSystemPro - VCM's custom built event registration and event 
+        management system.<br><br>
+
+        Built by event planners for event planners.
+      </p>
+
+  </div>
+</div>
+
+
+<div id="services" class="container" style="margin: 25px auto; color: #003366">
+  <!-- START THE FEATURETTES -->
+
+  <div class="col-lg-12" align="center" style="margin: 25px 0">
+    <p style="font-size: 5rem;">
+      Testimonials
+    </p>
+  </div>
+  <div class="col-lg-12">
+    <div class="row featurette">
+      <h2><img alt="" src="https://eventsystempro.com/uploads/images/delegate-testimonial-pic.png" style="float:right; height:250px; margin:0px 20px; width:250px" />Another satiated delegate...</h2>
+
+      <blockquote>
+        <p><span style="font-family:georgia,serif"><em>&quot;It was a great conference and valuable topics were addressed that I had never considered before. Thank you for putting it on! Plus the food was AMAZING!!!&quot;</em></span></p>
+
+        <p style="text-align:right"><span style="font-size:14px">Saskatchewan Public Sector Employee</span></p>
+
+        <p style="text-align:right"><span style="font-size:14px">2016 Saskatchewan Connections Conference</span></p>
+      </blockquote>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <h2><img alt="" src="/img/LewisEisen.jpg" style="float:left; height:250px; margin:0px 20px; width:250px" />Another satiated delegate...</h2>
+
+      <blockquote>
+        <p><span style="font-family:georgia,serif"><em>&quot;Verney Conference Management has been a pleasure to work with. They are responsive to my needs as an instructor, keeping me informed about potential issues or changes at every step along the way&quot;</em></span></p>
+
+        <p style="text-align:right"><span style="font-size:14px">Lewis S Eisen</span></p>
+
+        <p style="text-align:right"><span style="font-size:14px">Trainer • Author • Consultant</span></p>
+      </blockquote>
+
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <h2><img alt="" src="https://eventsystempro.com/uploads/images/delegate-testimonial-pic.png" style="float:right; height:250px; margin:0px 20px; width:250px" />Another satiated delegate...</h2>
+
+      <blockquote>
+        <p><span style="font-family:georgia,serif"><em>&quot;It was a great conference and valuable topics were addressed that I had never considered before. Thank you for putting it on! Plus the food was AMAZING!!!&quot;</em></span></p>
+
+        <p style="text-align:right"><span style="font-size:14px">Saskatchewan Public Sector Employee</span></p>
+
+        <p style="text-align:right"><span style="font-size:14px">2016 Saskatchewan Connections Conference</span></p>
+      </blockquote>
+    </div>
+
+    <!-- <hr class="featurette-divider"> -->
+
+  </div>
+  <!-- /END THE FEATURETTES -->
+</div>
+
+<div id="becomePartner" class="container-fluid" style="padding:2vw 0; margin: 25px auto;background-color: #003366; color: white " align="center">
+  <p style="font-size: 5rem; ">
+    Where we are today
+  </p>
+  <div class="col-lg-10" align="left" style="color: white;  text-align: justify; float: none; margin: 0 auto">
+    <div class="col-lg-6">
+      <p>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ‘Content here, content here’, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ‘lorem ipsum’ will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      </p>
+    </div>
+    <div class="col-lg-6">
+      <p>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ‘Content here, content here’, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ‘lorem ipsum’ will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      </p>
+    </div>
+  </div>
+</div>
+
+<div id="contact" class="container-fluid" style="padding:0; margin-top:25px;" align="center">
+
+  <!-- <div style="width: 70%; height: 3px; background-color: #003366"></div> -->
+
+  <div class="col-lg-12" style="color: #436990; margin: 25px auto">
+    <p>
+      <span style="font-size: 5rem; ">Contact Us</span><br><br><br>
+      If you have any questions or concerns, please contact us:<br><br>
+    </p>
+    <table>
+      <tr>
+        <td>Website:</td>
+        <td><a href="www.verney.ca">www.verney.ca</a></td>
+      </tr>
+      <tr>
+        <td>Telephone:</td>
+        <td>613.226.8317</td>
+      </tr>
+      <tr>
+        <td>Fax:</td>
+        <td>613.722.7725</td>
+      </tr>
+      <tr>
+        <td>Email:</td>
+        <td><a href="mailto:info@verney.ca">info@verney.ca</a></td>
+      </tr>
+    </table>
+    <p>
+      <br>2720 Queensview Dr.<br>
+      Ottawa, ON, Canada K2B 1A5
+    </p>
+  </div>
+
+
+  <div id="map" style="width:100%;height:500px; margin-bottom: 100px"></div>
+  <script>
+    function initMap() {
+      var uluru = {lat: 45.351881, lng: -75.788592};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    }
+  </script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZukZhiHna2oD9IlBqP0MDD5vCUJkVRqY&callback=initMap"
+  type="text/javascript"></script>
+
+</div>
+
+<div class="rTop" id="rTop" onClick="toTop()"><span class="glyphicon glyphicon-chevron-up" style="font-size: 20px; padding: 10px 0; 
+  " ></span></div>
+
+
